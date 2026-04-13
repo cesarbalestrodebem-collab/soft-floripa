0bbcde75c05e6ccdd1142a9cffbacc2f28d41bad
Cesar de Bem <cesarbalestrodebem@gmail.com>
2026-04-13 07:11:26 -0300

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
-import node from '@astrojs/node'
 
 export default defineConfig({
   site: 'https://softfloripa.com.br',
-  output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
   integrations: [
     sitemap(),
   ],
